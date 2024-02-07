041a68b4ac5db247d816d816490b8377f2bd1190
Álvaro Castillo <alvarolucascc96@gmail.com>
2024-02-07 01:07:20 -0300

    ADD: vistas configuración - modelo - carrera

--- modelos/urls.py
+++ modelos/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('modelos/', views.modelos),
     path('primer_anio/', views.primerAnio),
     path('carrera/', views.carrera),
+    path('toma_ramos/', views.tomaRamos),
+    path('asistencia_clases/', views.asistenciaClases),
+    path('notas_parciales/', views.notasParciales),
+    path('inasistencia_departamentales/', views.inasistenciaDepartamentales),
+    path('notas_departamentales/', views.notasDepartamentales),
+    path('rendimiento_previo/', views.rendimientoSemestrePrevio),
+    path('inasistencia_controles/', views.inasistenciaControles),
 ]
--- modelos/views.py
+++ modelos/views.py
@@ -11,3 +11,24 @@ def primerAnio(request):
 
 def carrera(request):
     return render(request, 'carrera.html')
+
+def tomaRamos(request):
+    return render(request, 'toma_ramos.html')
+
+def asistenciaClases(request):
+    return render(request, 'asistencia_clases.html')
+
+def inasistenciaDepartamentales(request):
+    return render(request, 'inasistencia_departamentales.html')
+
+def notasDepartamentales(request):
+    return render(request, 'notas_departamentales.html')
+
+def notasParciales(request):
+    return render(request, 'notas_parciales.html')
+
+def rendimientoSemestrePrevio(request):
+    return render(request, 'rendimiento_semestre_previo.html')
+
+def inasistenciaControles(request):
+    return render(request, 'inasistencia_controles.html')
