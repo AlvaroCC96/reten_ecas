098f6bae8a5d6017db4813419b4b684d09b12322
Álvaro Castillo <alvarolucascc96@gmail.com>
2024-02-15 23:58:12 -0300

    UPD : Modulo usuarios satra

--- usuariosSatra/views.py
+++ usuariosSatra/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 def usuariosSatra(request):
